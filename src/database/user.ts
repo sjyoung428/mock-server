7dcc928de229325caf559f944e50a7c3b244bf46
sjyoung428 <sjy8267@naver.com>
2023-04-27 23:57:41 +0900

    feat: signup method

---
+++ src/database/user.ts
@@ -0,0 +1,80 @@
+interface UserType {
+  id: number;
+  username?: string;
+  email: string;
+  password: string;
+  token: string;
+}
+
+export const user: UserType[] = [
+  {
+    id: 1,
+    username: "lirapi",
+    email: "lirapi@google.com",
+    password: "!h#]m9G@^=}#>H",
+    token: "2nq3kKjQFkyW8iJd3YX9n5xRjKdW8Zn5",
+  },
+  {
+    id: 2,
+    username: "julooi",
+    email: "julooi@naver.com",
+    password: "AaP7Vz>#",
+    token: "Cfa6oWp7Iu1sTmFyfYdIzTcV7TbT6UgZ",
+  },
+  {
+    id: 3,
+    username: "gevife",
+    email: "gevife@naver.com",
+    password: "{fjx77nqW#nx8DvG",
+    token: "z3w5r5r5v5r9c5f5r5r5y5h5d5g5f5",
+  },
+  {
+    id: 4,
+    username: "xoparo",
+    email: "xoparo@naver.com",
+    password: "ZM?DmKdq9)9u_#<W",
+    token: "Y2Q2NWI3NTlhNWIxZDlhODNlNTE5NTIz",
+  },
+  {
+    id: 5,
+    username: "zezoze",
+    email: "zezoze@google.com",
+    password: "4yWJ?6Ugpt6U9zHq",
+    token: "YmYwOGU0NTIyY2RjNGE2YjAxM2ViNWI1",
+  },
+  {
+    id: 6,
+    username: "luyilo",
+    email: "luyilo@naver.com",
+    password: "!uY7V{]M",
+    token: "MmZjNjAxYTA3ZjA3Z",
+  },
+  {
+    id: 7,
+    username: "jexexo",
+    email: "jexexo@naver.com",
+    password: "J=knY?+]#W",
+    token: "ZDEzNjJiMjBhZjRjODQ2Y2Y5YjY5ZjM4",
+  },
+  {
+    id: 8,
+    username: "riloto",
+    email: "riloto@google.com",
+    password: "u&z+8AK7V?bY6e<7",
+    token: "NmVhYzQ0Y2VjNzA1MmVjODA5MzY5N2Qy",
+  },
+  {
+    id: 9,
+    username: "wugaga",
+    email: "wugaga@naver.com",
+    password: "CzJ8@7PZd^wR",
+    token: "ZmM2Mzk1YzEzYmYxMzlhODkxMTZhNjMz",
+  },
+  {
+    id: 10,
+    username: "fememe",
+    email: "fememe@google.com",
+    password: "bJhH}3q^t?r&%vR%",
+    token: "NjJkMDVhMDRjMzQ1M2Q0ZjcwYzViOTY3",
+  },
+];
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,12 +1,41 @@
 import { rest } from "msw";
+import { user } from "../database/user";
+
+interface SignupBody {
+  email: string;
+  password: string;
+  username: string;
+}
+
+interface SignupResponse {
+  message: string;
+}
 
 export const handlers = [
-  rest.get("/", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        message: "Hello World!",
-      })
-    );
+  // sign up
+  rest.post<SignupBody, SignupResponse>("/api/signup", (req, res, ctx) => {
+    const { email, password, username } = req.body;
+    user.concat({
+      id: user.length + 1,
+      email,
+      password,
+      username,
+      token: "token",
+    });
+    if (email && password) {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          message: "Success",
+        })
+      );
+    } else {
+      return res(
+        ctx.status(400),
+        ctx.json({
+          message: "Invalid input",
+        })
+      );
+    }
   }),
 ];
